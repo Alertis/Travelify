41071a0fb81f2887fb82c2f6421594b649bf1d2a
Osman Yavuz Demir <osmanyavuzdemir96@gmail.com>
2020-10-31 20:13:58 +0300

    ui için react-native-paper paketi kuruldu

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Provider as PaperProvider, Button } from 'react-native-paper';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+      <Button icon="camera">
+        Press me
+      </Button>
+      </View>
+    </PaperProvider>
+   
   );
 }
 
