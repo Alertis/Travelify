9cecd60aeca350855a5790216bb20b45ba9acdd2
OsmanYavuzDemir <osmanyavuzdemir96@gmail.com>
2020-12-20 19:40:48 +0300

    maps stillendirildi

--- src/scenes/travels/components/Maps.js
+++ src/scenes/travels/components/Maps.js
@@ -1,27 +1,33 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, ScrollView, } from 'react-native';
 import {  IconButton, TextInput, Button } from 'react-native-paper';
-import MapView from 'react-native-maps';
+import MapView, { Marker, Callout} from 'react-native-maps';
 
 export default function Maps(props) {
     const [region, setRegion] = useState({
-        latitude: 37.78825,
-            longitude: -122.4324,
+        latitude: 40.9758824,
+            longitude: 29.070668,
             latitudeDelta: 0.0922,
             longitudeDelta: 0.0421,
     })
       
-      onRegionChange = (region)  => {
-        setRegion({ region });
-      }
+      
       
   return (
     <ScrollView style={styles.content}>
         <MapView
             style={styles.map}
             region={region}
-            onRegionChange={onRegionChange}
-        />
+        >
+            <Marker coordinate={region} >
+            <Callout>
+                <View>
+                    <Text> İstanbul Oyuncak Müzesi
+                    </Text>
+                </View>
+            </Callout>
+            </Marker>
+        </MapView>
     </ScrollView>
   );
 }
@@ -33,7 +39,7 @@ const styles = StyleSheet.create({
     map:{
         flex: 1,
         width: '100%',
-        height:100,
+        height: 500,
         backgroundColor:'red'
     }
 });
